16c7e516b3198a38e28c7a899d8170bd8d5fe315
deepak.dbhalla@gmail.com <deepak.dbhalla@gmail.com>
2023-11-02 19:06:27 +0530

    First commit

---
+++ src/test/java/com/example/security/SpringbootOauth2JwtAndBasicAuthSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOauth2JwtAndBasicAuthSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
